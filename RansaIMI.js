fbda344aca8e77614ff13cfce9a050b894d7a1ca
Taisuke Fukuno <fukuno@jig.jp>
2021-12-30 08:22:49 +0900

    add js

---
+++ RansaIMI.js
@@ -0,0 +1,9 @@
+export const privateKey = {
+  url: "https://rensadata.github.io/imi/#privateKey",
+  description: "64 byte private key by Ed25519",
+};
+
+export const publicKey = {
+  url: "https://rensadata.github.io/imi/#publicKey",
+  description: "32 byte public key by Ed25519",
+};
