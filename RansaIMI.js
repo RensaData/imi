ad769ba90280bf8a89e2e1e441eba86da0bc6ee1
Taisuke Fukuno <fukuno@jig.jp>
2021-12-30 08:26:40 +0900

    change data struct

--- RansaIMI.js
+++ RansaIMI.js
@@ -1,9 +1,10 @@
-export const privateKey = {
-  url: "https://rensadata.github.io/imi/#privateKey",
-  description: "64 byte private key by Ed25519",
-};
-
-export const publicKey = {
-  url: "https://rensadata.github.io/imi/#publicKey",
-  description: "32 byte public key by Ed25519",
+export const RensaIMI = {
+  privateKey: {
+    url: "https://rensadata.github.io/imi/#privateKey",
+    description: "64 byte private key by Ed25519",
+  },
+  publicKey: {
+    url: "https://rensadata.github.io/imi/#publicKey",
+    description: "32 byte public key by Ed25519",
+  },
 };
